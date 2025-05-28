d48bc07c95faf2bd3c2ebc45d084810910171b32
IrumShehryar <irum.shehryar@gmail.com>
2025-05-28 22:35:23 +0300

    added working simulation in main class and removed the extra user and system file

--- main.py
+++ main.py
@@ -219,29 +219,3 @@ if __name__ == "__main__":
     main()
 
 
-"""
-def main():
-    system = LostAndFoundSystem()
-    user = User("Iqra", "iqra@example.com", "123456789", "Student")
-    admin = Admin("Admin", "admin@example.com", "000000000")
-
-    # User reports a found item
-    found_item = Item("Phone", "Black iPhone", ItemType.FOUND, Category.ELECTRONIC, "Library", "iqra@example.com")
-    user.report_item(system, found_item)
-
-    # Another item reported as lost
-    lost_item = Item("Phone", "Black iPhone", ItemType.LOST, Category.ELECTRONIC, "Library", "xyz@example.com")
-    user.report_item(system, lost_item)
-
-    # Admin matches and resolves them
-    if admin.match_items(lost_item, found_item):
-        admin.resolve_items(lost_item, found_item)
-        print("Items matched and resolved.")
-
-    # Display final system items
-    for item in system.items:
-        print(f"{item.name} - {item.status.value}")
-
-if __name__ == "__main__":
-    main()
-"""
