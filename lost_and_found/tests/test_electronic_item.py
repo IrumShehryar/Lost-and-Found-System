792cda19b9eba8837cd4ee99d52423b3859a2981
sabaakbar858 <Blueberry@12345>
2025-05-30 13:37:57 +0500

    added detailed comment in test_electronic_item.py

--- lost_and_found/tests/test_electronic_item.py
+++ lost_and_found/tests/test_electronic_item.py
@@ -1,7 +1,7 @@
 import pytest
 from lost_and_found.enums import ItemType
 from lost_and_found.electronic_item import ElectronicItem
-
+# Define a test function to verify the behavior of the ElectronicItem class
 def test_electronic_item_fields():
     item = ElectronicItem("Laptop", "Gaming laptop", ItemType.LOST, "Library", "76876553", "HP", "Envy", "SN001")
     assert item.brand == "HP"
